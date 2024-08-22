c983b1a92040edaf0cfb0d6adf6d41918c53c884
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-08-22 18:49:52 +0300

    add json

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const TokuApp());
@@ -9,72 +10,85 @@ class TokuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color(0xffFEF6DB),
-        appBar: AppBar(
-          backgroundColor: const Color(0xff46322B),
-          title: const Text(
-            'Touk',
-            style: TextStyle(color: Colors.white),
-          ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffFEF6DB),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff46322B),
+        title: const Text(
+          'Touk',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Column(
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              height: 65,
-              width: double.infinity,
-              color: const Color(0xffEf9235),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Nembers ',
-                  style: TextStyle(fontSize: 22, color: Colors.white),
-                ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: const Color(0xffEf9235),
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Nembers ',
+                style: TextStyle(fontSize: 22, color: Colors.white),
               ),
             ),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                alignment: Alignment.centerLeft,
-                height: 65,
-                width: double.infinity,
-                color: const Color(0xff558B37),
-                child: const Text(
-                  'Family Membres',
-                  style: TextStyle(fontSize: 22, color: Colors.white),
-                ),
-              ),
-            ),
-            Container(
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
               padding: const EdgeInsets.all(16),
               alignment: Alignment.centerLeft,
               height: 65,
               width: double.infinity,
-              color: const Color(0xff79359F),
+              color: const Color(0xff558B37),
               child: const Text(
-                'Color ',
+                'Family Membres',
                 style: TextStyle(fontSize: 22, color: Colors.white),
               ),
             ),
-            Container(
-              alignment: Alignment.centerLeft,
-              height: 65,
-              width: double.infinity,
-              color: const Color(0xff50ADC7),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Phrases ',
-                  style: TextStyle(fontSize: 22, color: Colors.white),
-                ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: const Color(0xff79359F),
+            child: const Text(
+              'Color ',
+              style: TextStyle(fontSize: 22, color: Colors.white),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: const Color(0xff50ADC7),
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Phrases ',
+                style: TextStyle(fontSize: 22, color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Lottie.asset('assets/images/json_svg/Home_Screen.json',
+                height: 250),
+          ),
+        ],
       ),
     );
   }
